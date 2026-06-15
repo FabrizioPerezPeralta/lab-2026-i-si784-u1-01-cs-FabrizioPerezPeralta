1e6fa9d9d1b2804a472c0d6918ae8a32045e5083
FabrizioPerezPeralta <fp2023077476@virtual.upt.pe>
2026-06-15 11:19:21 -0500

    Laboratorio 1: Sonar

---
+++ Bank.Domain.Tests/BankAccountTests.cs
@@ -0,0 +1,39 @@
+using Bank.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank.Domain.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 16.54;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            account.Credit(creditAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+    }
+}
